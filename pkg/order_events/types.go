efce9e4c73b3fd72b3f5b65b53d4e20811dbcccf
Alex V Kotelnikov <gudron2s@gmail.com>
2022-08-25 22:04:27 +0400

    added deposit order created event type

--- pkg/order_events/types.go
+++ pkg/order_events/types.go
@@ -5,34 +5,37 @@ type EventType uint8
 const (
 	EventTypeDefault EventType = iota + 1
 	EventTypeOrderAccepted
-	EventTypeOrderCreated
+	EventTypeWithdrawOrderCreated
 	EventTypeCancelOrder
 	EventTypeOrderFailed
 	EventTypeBcTxFound
 	EventTypeReOrder
 	EventTypeOrderSuccessfullyProcessed
+	EventTypeDepositOrderCreated
 )
 
 const (
 	EventTypeDefaultName                    = "event_default"
 	EventTypeAcceptedOrderName              = "event_order_accepted"
-	EventTypeCreatedOrderName               = "event_order_created"
+	EventTypeCreatedWithdrawOrderName       = "event_withdraw_order_created"
 	EventTypeCancelOrderName                = "event_cancel_order"
 	EventTypeOrderFailedName                = "event_order_failed"
 	EventTypeBcTxFoundName                  = "event_bc_tx_found"
 	EventTypeReOrderName                    = "event_re_order"
 	EventTypeOrderSuccessfullyProcessedName = "event_order_successfully_processed"
+	EventTypeCreatedDepositOrderName        = "event_deposit_order_created"
 )
 
 func (d EventType) String() string {
 	return [...]string{"",
 		EventTypeDefaultName,
 		EventTypeAcceptedOrderName,
-		EventTypeCreatedOrderName,
+		EventTypeCreatedWithdrawOrderName,
 		EventTypeCancelOrderName,
 		EventTypeOrderFailedName,
 		EventTypeBcTxFoundName,
 		EventTypeReOrderName,
 		EventTypeOrderSuccessfullyProcessedName,
+		EventTypeCreatedDepositOrderName,
 	}[d]
 }
